151fe0f6ab403e3f4cec5845efd76d8326506d37
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2024-07-17 03:38:01 -0700

    Update Home app

--- Home/views.py
+++ Home/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def view_home_page(reqest):
-    pass
+def view_home_page(request):
+    if request.method == "POST":
+        pass
+
+    return render(request=request)
