f0b74fd09bf12b4aa9613c06fa2bf30abebe6621
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2024-07-17 04:20:39 -0700

    Update Content & Card apps

--- Card/models.py
+++ Card/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Content.models import Product
 
-# Create your models here.
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def full_price(self):
+        total_price = sum(product.price for product in self.products.all())
+        return total_price
--- Content/models.py
+++ Content/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
     price = models.DecimalField(decimal_places=2, max_digits=10)
 
+    def __str__(self):
+        return self.name
 
-class Image_Product(models.Model):
+
+class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="media/", max_length=20)
+    image = models.ImageField(upload_to="media/")
     default_image = models.BooleanField(default=False)
 
     def __str__(self):
