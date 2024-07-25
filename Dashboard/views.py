a0cacf925d778f5088c39df21ce083e4f5e2f7df
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2024-07-25 12:39:42 -0700

    Update views.py

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -20,19 +20,27 @@ def view_dashboard(request):
 @login_required
 def upgrade_to_seller(request):
 
-    if request.method == "POST":
-        store_name = request.POST.get("store_name")
-        store_description = request.POST.get("store_description")
+    user = request.user
+    type_user = user.type_user
 
-        if store_name and store_description:
-            user = request.user
-            user.type_user = "seller"
-            user.store_name = store_name
-            user.store_description = store_description
-            user.save()
-            return redirect("dashboard")
+    if type_user == "normal":
 
-        else:
-            context = {"error": "All fields is required."}
+        if request.method == "POST":
+            store_name = request.POST.get("store_name")
+            store_description = request.POST.get("store_description")
+
+            if store_name and store_description:
+                user = request.user
+                user.type_user = "seller"
+                user.store_name = store_name
+                user.store_description = store_description
+                user.save()
+                return redirect("dashboard")
+
+            else:
+                context = {"error": "All fields is required."}
+
+    else:
+        context = {"error": "User Type must be Normal."}
 
     return render(request, "normal_to_seller.html", context)
